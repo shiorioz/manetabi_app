430787aae85ca7bd82eb5d99733cb82744353674
shiorioz <ozws011.tmt@gmail.com>
2023-07-17 11:32:19 +0900

    create: project files

---
+++ lib/model/users_model.dart
@@ -0,0 +1,12 @@
+// class User with _$User {
+//   const factory User({
+//     required user_id,
+//     required user_name,
+//     required email,
+//     required password,
+//     required registration_date,
+//     profile_image_path,
+//     required created_at,
+//     required updated_at,
+//   }) = _User;
+// }
